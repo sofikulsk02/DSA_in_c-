47ecf7fe7bbc6b5b82da0a1ec993e375a01910db
SOFIKUL SK <rajibsk@gmail.com>
2025-05-18 17:21:45 +0530

    chekcsum sub set without extra space

---
+++ BitManupilation/checkSum.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool checkSum(int arr[], int n,int k){
+     for(int i=0; i<(1<<n);i++){
+        int sum=0;
+        for(int j=0; j<n;j++){
+            if((i>>j&1)==1){
+                //the bit is set we have to consider that 
+               sum+=arr[j];
+            }
+        }
+        if(sum==k) return true;
+    }
+    return false;
+}
+int main(){
+    int arr[]={2 ,3, -6, 3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int k=9;
+   cout<<checkSum(arr,n,k);
+}
