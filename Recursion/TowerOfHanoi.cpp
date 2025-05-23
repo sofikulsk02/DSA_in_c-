1d7db18fd1839c3ad6ab5217f61aa514ab1aecd2
SOFIKUL SK <rajibsk563@gmail.com>
2025-05-23 23:51:35 +0530

    tower of hanoi

---
+++ Recursion/TowerOfHanoi.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+void towerOfHanoi(int n,char a,char b,char c){
+    if(n==0) return;
+    towerOfHanoi(n-1,a,c,b);
+    cout<<a<<"-->"<<c<<endl;
+    towerOfHanoi(n-1,b,a,c);
+}
+int main(){
+    int n=4;
+    towerOfHanoi(n,'A','B','C');
+}
