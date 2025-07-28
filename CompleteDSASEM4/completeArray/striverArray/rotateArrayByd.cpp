df569bb739a4ddce840e19d2b6b5ef6f29279c8d
SOFIKUL SK <rajibsk563@gmail.com>
2025-07-28 11:11:45 +0530

    left rotate all the approaches

---
+++ CompleteDSASEM4/completeArray/striverArray/rotateArrayByd.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+void rotateByd(int nums[],int d,int n){
+  int temp [d];
+  for(int i=0;i<d;i++){
+        temp[i]=nums[i];
+  }
+  //shifting the elements
+  for(int i=d;i<n;i++){
+    nums[i-d]=nums[i];
+  }
+  //putting the temp elements into the array
+  for(int i=n-d;i<n;i++){
+    nums[i]=temp[i-(n-d)];
+  }
+}
+int main(){
+    int nums[]={1,2,3,4,5,6,7};
+    int n=7;
+    int d=3;
+    rotateByd(nums,d,n);
+    for(int i=0;i<n;i++){
+        cout<<nums[i]<<" ";
+    }
+}
